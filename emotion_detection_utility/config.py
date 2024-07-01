10dba684078eb32ca4f503a404a54e546952ca9f
Atif <akakhtarsbg@gmail.com>
2024-07-01 11:14:23 +0530

    file rename

--- emotion_detection_utility/config.py
+++ emotion_detection_utility/config.py
@@ -1,8 +1,8 @@
 import torch
 
 ENCODING_PATH = "D:/My stuffs/0Study material/SEMESTER/CMED_Internship/encodings.pickle"
-VIDEO_FOLDER_PATH = "D:/My stuffs/0Study material/SEMESTER/CMED_Internship/custom_dataset/test_dia64/"
-EXCEl_PATH = "custom_dataset/test_dia64.xlsx"
+VIDEO_FOLDER_PATH = "/self_reporting_model/test_dia64/"
+EXCEl_PATH = "self_reporting_model/test_dia64.xlsx"
 
 DETECTION_METHOD = "hog"
 DISPLAY = 1
